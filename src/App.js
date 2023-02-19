1623793750098e5ea42035c35eb8f0084dae19ff
magdalenahuget <magdalena.huget@gmail.com>
2023-02-19 14:53:03 +0100

    Add functionality slider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import CounterButton from './components/CounterButton/CounterButton';
 import PokemonView from './components/PokemonView/PokemonView';
 import {useState} from 'react';
+import {Slider} from '@mui/material';
 
 function App() {
   const [counter, setCounter] = useState(1);
@@ -14,8 +15,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <PokemonView pokemonId={counter}/>
         <CounterButton counter={counter} onClick={onClick} />
-
-
+        <div className="mySlider">
+          <Slider defaultValue={30} 
+          aria-label="Disabled slider" 
+          onChange={e => setCounter(e.target.value)} />
+        </div>
       </header>
     </div>
   );
